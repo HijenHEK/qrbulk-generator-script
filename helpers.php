cf68b4a613fcb6307485cdc348df658eb767c0f1
Hijen <hijenhek@gmail.com>
2021-11-21 10:36:45 +0100

    txt input file

--- helpers.php
+++ helpers.php
@@ -1,14 +1,58 @@
 <?php 
 
-
-
+// clean name for file or directory creation
 function sanitize_name($name) {
+    $name = trim($name);
     return preg_replace("([^\w\s\d\-_~,;\[\]\(\)])" , "" ,  $name);
 }
+
+// check if the name is clean for file or directory creation
+function is_sanitized($name) {
+    return $name == sanitize_name($name) ;
+}
+
+// get file name only
+function get_file_name($file) {
+    $dot_pos = strpos($file, '.' );
+    return $dot_pos ? substr($file ,0, $dot_pos) : $file;
+}
+// get extension only
+function get_file_extension($file) {
+    $dot_pos = strpos($file, '.' );
+    return $dot_pos ? substr($file , $dot_pos) : null;
+}
+// check if file is txt
+function is_txt($file) {
+    get_file_extension($file) == 'txt' ;
+}
+// create a folder and add time int if exists already 
+function create_folder_from($file) {
+    $folder_name = get_file_name($file);
+    if(file_exists($folder_name)){ mkdir($folder_name . time() . rand(0,500)); return $folder_name ;}
+    mkdir($folder_name); return $folder_name;
+}
+
+// clean file name ( with extension )
 function sanitize_file_name($file_name){
     $dot_pos = strpos($file_name, '.' );
     $only_name = substr($file_name ,0, $dot_pos);
     $extension = substr($file_name , $dot_pos);
     $only_name_clean = sanitize_name($only_name);
     return str_replace(' ','', $only_name_clean) . $extension;
+}
+
+// read line from a txt file and return a qr code array
+function read_from_file(String $file) : array{
+    $handle = fopen($file, "r");
+    $array = [];
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            array_push($array , $line);
+        }
+
+        fclose($handle);
+    } else {
+        die('error while reading from file ' . $file);
+    }
+    return $array;
 }
--- index.php
+++ index.php
@@ -5,12 +5,26 @@ use chillerlan\QRCode\QROptions;
 
 require "./vendor/autoload.php";
 require "./helpers.php";
-
 $codes = [
-    "*qr ;; value 1",
+    "*qr ;; value 1,",
     "qr value 2",
     "qr value 3",
  ];
+
+$output_folder  = 'results';
+
+if($argc == 2) {
+    $input_file = $argv[1];
+    if(is_txt($input_file)) { die('not a txt file ? please try a gain with a .txt file !') ;}
+    if(!file_exists($input_file)) {
+        die('file dosn\'t exist !');
+    }
+    $output_folder = create_folder_from($input_file);
+    $codes = [];
+    $codes = read_from_file($input_file);
+
+}
+
 
 
 
@@ -20,6 +34,10 @@ $options = new QROptions([
     'eccLevel'   => QRCode::ECC_L,
 ]);
 
+if( !file_exists($output_folder)) {
+    mkdir($output_folder);
+}
+
 foreach($codes as $code) {
     
 
@@ -27,16 +45,10 @@ foreach($codes as $code) {
     // invoke a fresh QRCode instance
     $qrcode = new QRCode($options);
     
-    // and dump the output
-    $qrcode->render( $code );
-    
-    // ...with additional cache file
-    if( !file_exists('results')) {
-        mkdir('results');
-    }
+
     // use file name sanitizer helper function 
 
     $filename = sanitize_name($code);
 
-    $qrcode->render($code , 'results/'. $filename.'.png');
+    $qrcode->render($code , $output_folder.'/'. $filename.'.png');
 }
